7cbb09cb9afcc1b96b26010c6711d9d6a54ffc6a
Nicola <DALPOZZO92@GMAIL.COM>
2025-12-11 23:06:16 +0100

    test tabbar

--- packages/mobile/dev-dist/sw.js
+++ packages/mobile/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-10f05c2d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.phqichpt3i8"
+    "revision": "0.5d04fl16098"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
